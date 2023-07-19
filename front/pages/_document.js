601af4e11d5068f0e5453fbeceb15a17566e80cf
devseop <cocc263@gmail.com>
2023-07-19 18:36:09 +0900

    :art: [Ch5] SSR for Styled-Components
    SSR 환경에서의 CSS 적용 방법과 폴리필에 대해 알아보고 이를 프로젝트에 적용해보는 실습을 진행

---
+++ front/pages/_document.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  //* getInitialProps는 _app이나 _document에서만 쓰는 특수한 SSR Method
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } catch (error) {
+      console.error(error);
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <script src="https://polyfill.io/v3/polyfill.min.js?features=default%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces2019%2Ces2020%2Ces2021%2Ces2022" />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
