3d8fefbc80eafb718f83748e152c4f2c4b77137b
devseop <cocc263@gmail.com>
2023-07-20 23:20:48 +0900

    :art: [Ch5] Custom Webpack & bundle-analyzer

--- front/next.config.js
+++ front/next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer({
+  compress: true,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === "production";
+
+    return {
+      ...config,
+      mode: prod ? "production" : "development",
+      devtool: prod ? "hidden-source-map" : "eval",
+      plugins: [...config.plugins],
+    };
+  },
+});
